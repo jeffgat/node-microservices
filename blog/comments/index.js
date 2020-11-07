3fb2c1c6e04cdac48e20d94d626ac693e850f8a1
jeffreygatbonton <jeff@creationcrate.com>
2020-11-07 12:56:21 -0500

    restructuring

---
+++ blog/comments/index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(bodyParser.json());
+
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []) 
+});
+
+app.post('/posts/:id/comments', async (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
+  const { content } = req.body;
+
+  // initialize comments by getting the post id
+  const comments = commentsByPostId[req.params.id] || [];
+
+  // push the comments under that post id
+  comments.push({ id: commentId, content });
+
+  // set that object on the commentsByPostId object
+  commentsByPostId[req.params.id] = comments;
+
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id
+    }
+  })
+
+  res.status(201).send(comments);
+});
+
+app.post('/events', (req, res) => {
+  console.log('Received Event', req.body.type);
+
+  res.send({});
+});
+
+app.listen(4001, () => {
+  console.log('Listening on 4001');
+});
